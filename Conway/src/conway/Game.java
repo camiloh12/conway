49f90d5586f97ee6cb2b2588f807f6cad5bcedee
camiloh12 <camiloh1286@gmail.com>
2015-11-24 10:16:53 -0800

    Simplified the boolean logic which applies the game's rules.

--- Conway/src/conway/Game.java
+++ Conway/src/conway/Game.java
@@ -10,10 +10,8 @@ public class Game {
 		for (int i = 0; i <= grid.cells.length - 1; i++) {
 			for (int j = 0; j <= grid.cells[i].length - 1; j++) {
 				int liveNeighbors = grid.getNumberOfLiveNeighbors(i, j);
-				if (liveNeighbors >= 2 && liveNeighbors <= 3) {
-					if (grid.cells[i][j] || liveNeighbors == 3) {
-						tempCells[i][j] = true;
-					}
+				if (grid.cells[i][j] && liveNeighbors == 2 || liveNeighbors == 3) {
+					tempCells[i][j] = true;
 				} else {
 					tempCells[i][j] = false;
 				}
