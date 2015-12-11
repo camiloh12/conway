c8f313e7b3b4c4e3c9ff0a8ee8d3bfbe1cfe49ef
camiloh12 <camiloh1286@gmail.com>
2015-12-11 12:45:34 -0500

    Added out of bounds check to avoid catching indexOutOfBounds exception.

--- Conway/src/main/java/conway/Grid.java
+++ Conway/src/main/java/conway/Grid.java
@@ -1,10 +1,10 @@
 package conway;
 
 public class Grid {
-	
+
 	private int size;
 	public boolean[][] cells;;
-	
+
 	public Grid(int size) {
 		this.size = size;
 		cells = new boolean[size][size];
@@ -14,27 +14,34 @@ public class Grid {
 		int count = 0;
 		for (int i = x - 1; i <= x + 1; i++) {
 			for (int j = y - 1; j <= y + 1; j++) {
-				try {
-					if (cells[i][j] && !(i == x && j == y)) {
-						count++;
-					}
-				} catch (IndexOutOfBoundsException iob) {
-					// do nothing
+				if (!isOutOfBounds(i, j) && !isSameCell(x, y, i, j) && cells[i][j]) {
+					count++;
 				}
 			}
 		}
 		return count;
 	}
-	
+
 	public String toString() {
 		StringBuilder gridString = new StringBuilder();
-		for (int i = 0; i <= size - 1; i++) {
-			for (int j = 0; j <= size - 1; j++) {
-				gridString.append(cells[j][i] ? "O" : " ");
+		for (int y = 0; y <= size - 1; y++) {
+			for (int x = 0; x <= size - 1; x++) {
+				gridString.append(cells[x][y] ? "@" : " ");
 			}
 			gridString.append("\n");
 		}
-		
+
 		return gridString.toString();
+	}
+
+	private boolean isOutOfBounds(int x, int y) {
+		if (x < 0 || x >= size || y < 0 || y >= size) {
+			return true;
+		}
+		return false;
+	}
+
+	private boolean isSameCell(int cellX, int cellY, int neighborX, int neighborY) {
+		return neighborX == cellX && neighborY == cellY;
 	}
 }
--- Conway/src/test/java/conway/GridTest.java
+++ Conway/src/test/java/conway/GridTest.java
@@ -1,14 +1,17 @@
 package conway;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class GridTest {
-	
+
 	private Grid grid;
-	
+
 	@Before
 	public void setUp() {
 		grid = new Grid(5);
@@ -16,40 +19,42 @@ public class GridTest {
 
 	@Test
 	public void testGrid() {
-		assertNotNull(grid.cells[0][0]);
+		assertNotNull(grid.cells[4][4]);
 	}
-	
+
 	@Test
 	public void testAlive() {
 		grid.cells[0][0] = true;
-		
+
 		assertTrue(grid.cells[0][0]);
+		assertFalse(grid.cells[4][4]);
 	}
-	
+
 	@Test
 	public void testNumberOfLiveNeighbors() {
 		grid.cells[2][2] = true;
 		grid.cells[3][2] = true;
-		int liveNeighbors = grid.getNumberOfLiveNeighbors(2,3);
+		int liveNeighbors = grid.getNumberOfLiveNeighbors(2, 3);
 		assertEquals(2, liveNeighbors);
 	}
+
 	@Test
 	public void testLiveNeighborsWithLiveCell() {
 		grid.cells[2][2] = true;
 		grid.cells[3][2] = true;
 		grid.cells[2][3] = true;
-		int liveNeighbors = grid.getNumberOfLiveNeighbors(2,3);
+		int liveNeighbors = grid.getNumberOfLiveNeighbors(2, 3);
 		assertEquals(2, liveNeighbors);
 	}
-	
+
 	@Test
 	public void testOutOfBounds() {
 		grid.cells[0][1] = true;
 		grid.cells[1][0] = true;
-		int liveNeighbors = grid.getNumberOfLiveNeighbors(0,0);
+		int liveNeighbors = grid.getNumberOfLiveNeighbors(0, 0);
 		assertEquals(2, liveNeighbors);
 	}
-	
+
 	@Test
 	public void testLiveNeighbors4() {
 		grid.cells[2][3] = true;
@@ -57,16 +62,16 @@ public class GridTest {
 		grid.cells[1][3] = true;
 		grid.cells[2][2] = true;
 		grid.cells[3][2] = true;
-		assertEquals(4, grid.getNumberOfLiveNeighbors(2,3));
+		assertEquals(4, grid.getNumberOfLiveNeighbors(2, 3));
 	}
-	
+
 	@Test
 	public void testToString() {
 		grid.cells[0][2] = true;
 		grid.cells[0][3] = true;
 		grid.cells[1][2] = true;
 		grid.cells[1][3] = true;
-		String gridString = "     \n     \nOO   \nOO   \n     \n";
+		String gridString = "     \n     \n@@   \n@@   \n     \n";
 		assertEquals(gridString, grid.toString());
 	}
 
